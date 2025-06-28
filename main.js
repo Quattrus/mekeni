ce6f1fc9b128ee22b90b7294db3fa3f3d7512b1c
Quartus <94739286+Quattrus@users.noreply.github.com>
2025-06-28 16:39:26 +0800

    replaced skypack

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://cdn.skypack.dev/three';
+import * as THREE from 'https://esm.sh/three@0.155.0';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
